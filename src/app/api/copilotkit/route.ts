02dad2af31ef813b7837b0a667c281d37a6707ec
Markus Ecker <markus.ecker@gmail.com>
2025-07-22 13:52:40 +0200

    use env vars for urls

--- src/app/api/copilotkit/route.ts
+++ src/app/api/copilotkit/route.ts
@@ -7,15 +7,15 @@ import { NextRequest } from "next/server";
 import { A2AClientAgent } from "./a2a";
 import { openai } from "@ai-sdk/openai";
 
+const A2A_URL_1 = process.env.A2A_URL_1 || "http://127.0.0.1:9999";
+const A2A_URL_2 = process.env.A2A_URL_2 || "http://127.0.0.1:9998";
+const A2A_URL_3 = process.env.A2A_URL_3 || "http://127.0.0.1:9997";
+
 const runtime = new CopilotRuntime({
   agents: {
     a2a_chat: new A2AClientAgent({
       model: openai("gpt-4o", { parallelToolCalls: false }),
-      agentUrls: [
-        "http://127.0.0.1:9999",
-        "http://127.0.0.1:9998",
-        "http://127.0.0.1:9997",
-      ],
+      agentUrls: [A2A_URL_1, A2A_URL_2, A2A_URL_3],
       instructions: `
           You are an HR agent. You are responsible for hiring employees and other typical HR tasks.
 
